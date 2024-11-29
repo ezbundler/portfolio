ab26fe1b5a5f78d2287375df9918c3655a9662dc
ezbundler <developer.akash1236@gmail.com>
2024-11-29 09:15:00 +0530

    new changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
         <motion.h2 variants={childVarients} className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Akash Chaudhary</motion.h2>
         <motion.span variants={childVarients} className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent '>Full Stack Developer</motion.span>
         <motion.p variants={childVarients} className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter' >{HERO_CONTENT}</motion.p>
-        <motion.a variants={childVarients} href='/resume.pdf' target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>Download Resume</motion.a>
+        <motion.a variants={childVarients} href='/Akash_Chaudhary_Developer_Resume.pdf' target='_blank' rel='noopener noreferrer' download className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>Download Resume</motion.a>
     </motion.div>
     </div>
     </div>
